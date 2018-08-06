7e50480020bc6b755f4bb82b0886cad777bc844c
Gabriel Ng <gabrielng15@gmail.com>
2018-08-06 15:47:43 -0700

    updated About and Archive components

--- src/Index/Index.js
+++ src/Index/Index.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Header from "./Header/Header";
+import Archive from "./Archive/Archive";
+import About from "./About/About";
 
 class Index extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Archive />
+        <About />
       </div>
     );
   }
